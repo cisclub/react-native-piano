0f40434cf93b4370dd3ddc1d319e990aec95e87e
aliamin <aamin@gulfnews.com>
2020-09-14 13:52:09 +0400

    add native module to index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import { NativeModules } from 'react-native';
 
-const { PianoComposer } = NativeModules;
+const PianoComposerModule = NativeModules.PianoComposer;
+
+const PianoComposer = {
+    execute(aid: String,
+     sandbox: bool = true,
+     tags: Array = null,
+     zoneID: String = null,
+     referrer: String = null,
+     url: String = null,
+     customVariables: Dictionary = null,
+     userToken: String = null) {
+        PianoComposerModule.execute(aid, sandbox, tags, zoneID, referrer, url, customVariables, userToken);
+    }
+}
 
 export default PianoComposer;
